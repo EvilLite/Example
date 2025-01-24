fd5b7c4195d843589a159ae5abf7118fd37a1e26
Alex Grigol <alexgrigol@Alexs-MacBook-Air.local>
2025-01-24 21:53:38 +0300

    delete extra indentation

---
+++ exampleApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  exampleApp
+//
+//  Created by Alex Grigol on 24/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
